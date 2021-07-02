ccb03e632d003cbe1337e6498922da4b5ca1312d
Yuliia Matvi <jualiasha@gmail.com>
2021-07-02 16:20:51 +0300

    find month-labels in chart

--- dashboard/src/Components/Shared/ChartLine.tsx
+++ dashboard/src/Components/Shared/ChartLine.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { FC, useState } from 'react';
 import { Line } from 'react-chartjs-2';
 
-const state = {
+const stateex = {
 	labels: ['January', 'February', 'March', 'April', 'May'],
 	datasets: [
 		{
@@ -17,26 +17,52 @@ const state = {
 	],
 };
 
-export default class App extends React.Component {
-	render() {
-		return (
-			<div>
-				<Line
-					type={'line'}
-					data={state}
-					options={{
-						title: {
-							display: true,
-							text: 'Average Rainfall per month',
-							fontSize: 20,
-						},
-						legend: {
-							display: true,
-							position: 'right',
-						},
-					}}
-				/>
-			</div>
-		);
-	}
-}
+const ChartLine: FC = () => {
+	const [state, setState] = useState({});
+
+	const findmonth = () => {
+		/* const currentdate = new Date(); */
+		/* const currentmonth = month[currentdate.getMonth()]; */
+		const month = ['Jan', 'Feb', 'March', 'Apr', 'May', 'June', 'July', 'August', 'Sept', 'Oct', 'Nov', 'Dec'];
+		let chartmonthend = [];
+		let chartmonthstart = [];
+		const history = 9; /*how many months before to show + current month*/
+		const startmonth = new Date(new Date().getFullYear(), new Date().getMonth() - history, new Date().getDate()).getMonth();
+
+		const endgap = month.length - startmonth;
+		const startgap = history - endgap;
+
+		for (let i = startmonth; i < month.length; i++) {
+			chartmonthend.push(month[i]);
+		}
+
+		for (let i = 0; i < startgap + 1; i++) {
+			chartmonthstart.push(month[i]);
+		}
+		let chartmonths = chartmonthend.concat(chartmonthstart);
+		console.log(chartmonths);
+	};
+	return (
+		<div>
+			<Line
+				type={'line'}
+				data={stateex}
+				options={{
+					title: {
+						display: true,
+						text: 'Monthly interactions',
+						fontSize: 20,
+					},
+					legend: {
+						display: true,
+						position: 'right',
+						padding: 20,
+						boxwidth: 100,
+					},
+				}}
+			/>
+		</div>
+	);
+};
+
+export default ChartLine;
