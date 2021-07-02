8ef65b065e828c4b48f94636e238129fd5c2fdc8
Yuliia Matvi <jualiasha@gmail.com>
2021-07-03 00:50:07 +0300

    skeleton for automation started

--- dashboard/src/Components/Main/Automation.tsx
+++ dashboard/src/Components/Main/Automation.tsx
@@ -1,7 +1,73 @@
 import React, { FC } from 'react';
+import { AddCircleOutline, Edit, DeleteOutline, FiberManualRecord } from '@material-ui/icons';
 
 const Automation: FC = () => {
-	return <div>This is for automation</div>;
+	return (
+		<section id="automation">
+			<section className="category-list">
+				<h4>
+					Categories <AddCircleOutline style={{ fontSize: 30 }} className="add-icon" />
+				</h4>
+				<div className="single-category">
+					Internet
+					<div className="button-section">
+						<button className="edit-button">
+							<Edit style={{ fontSize: 25 }} />
+						</button>
+						<button className="delete-button">
+							<DeleteOutline style={{ fontSize: 25 }} />
+						</button>
+					</div>
+				</div>
+				<div className="single-category">
+					Telephone
+					<button className="edit-button">
+						<Edit style={{ fontSize: 25 }} />
+					</button>
+					<button className="delete-button">
+						<DeleteOutline style={{ fontSize: 25 }} />
+					</button>
+				</div>
+				<div className="single-category">
+					Business
+					<button className="edit-button">
+						<Edit style={{ fontSize: 25 }} />
+					</button>
+					<button className="delete-button">
+						<DeleteOutline style={{ fontSize: 25 }} />
+					</button>
+				</div>
+				<div className="single-category">
+					Private
+					<button className="edit-button">
+						<Edit style={{ fontSize: 25 }} />
+					</button>
+					<button className="delete-button">
+						<DeleteOutline style={{ fontSize: 25 }} />
+					</button>
+				</div>
+			</section>
+			<section className="topics-list">
+				<ul>
+					<li>
+						<FiberManualRecord /> How to connect Telia
+					</li>
+					<li>
+						<FiberManualRecord /> How to connect Telia
+					</li>
+					<li>
+						<FiberManualRecord /> How to connect Telia
+					</li>
+					<li>
+						<FiberManualRecord /> How to connect Telia
+					</li>
+					<li>
+						<FiberManualRecord /> How to connect Telia
+					</li>
+				</ul>
+			</section>
+		</section>
+	);
 };
 
 export default Automation;
