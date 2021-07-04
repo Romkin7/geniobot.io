b16b7037add7a8179ea52b5f17dd7790c04a0a7f
Yuliia Matvi <jualiasha@gmail.com>
2021-07-04 23:26:04 +0300

    conflicts resolved

--- dashboard/src/Components/Main/Intellect.tsx
+++ dashboard/src/Components/Main/Intellect.tsx
@@ -1,7 +1,25 @@
 import React, { FC } from 'react';
+import { Edit, DeleteOutline } from '@material-ui/icons';
 
 const Intellect: FC = () => {
-	return <div>This is for art.intellect content</div>;
+	return (
+		<section id="intellect">
+			<p>
+				Artificial Intellect is a feature to remember questions that bot couldn't answer. It will remember them automatically. Also
+				you can add them here manually or with right click from Messages section
+			</p>
+			<div className="question">
+				Where can i buy telephone card?
+				<Edit style={{ fontSize: 30 }} />
+				<DeleteOutline style={{ fontSize: 30 }} />
+			</div>
+			<div className="question">
+				Where can i buy telephone card?
+				<Edit style={{ fontSize: 30 }} />
+				<DeleteOutline style={{ fontSize: 30 }} />
+			</div>
+		</section>
+	);
 };
 
 export default Intellect;
