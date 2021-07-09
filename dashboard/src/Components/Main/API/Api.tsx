56ceed98ec663e2dd9f4eb935a95f57366c2421c
Yuliia Matvi <jualiasha@gmail.com>
2021-07-09 15:05:50 +0300

    api main styles+functions

--- dashboard/src/Components/Main/API/Api.tsx
+++ dashboard/src/Components/Main/API/Api.tsx
@@ -1,11 +1,25 @@
 import React, { FC } from 'react';
 import ApiBox from './ApiBox';
+import { AddCircleOutline } from '@material-ui/icons';
 
 const Api: FC = () => {
+	const colors: string[] = ['aquagreen', 'lavender', 'dazzlerose'];
+	/* const forcolor = () => {
+		for (let i = 0; i < colors.length; i++) {
+			return colors[i];
+		}
+	}; */
 	return (
 		<section id="api-list">
 			<p>API-integrations list is a list of API address from where you want to take info to your chatbot</p>
-			<ApiBox address="http://backend-app.io/data" name="balance" />
+			<h4>
+				ADD API
+				<button>
+					<AddCircleOutline style={{ fontSize: 30, position: 'relative', top: '1.3vh', marginLeft: '1vw' }} />
+				</button>
+			</h4>
+			<ApiBox address="http://backend-app.io/data" name="balance" maincolor={colors[Math.floor(Math.random() * colors.length)]} />
+			<ApiBox address="http://backend-app.io/data" name="balance" maincolor={colors[Math.floor(Math.random() * colors.length)]} />
 		</section>
 	);
 };
