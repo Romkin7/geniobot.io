2a0b36d23f226a621105f311eeaf9dc6e283640d
Yuliia Matvi <jualiasha@gmail.com>
2021-07-22 23:27:47 +0300

    conditional rendering for buttons added

--- dashboard/src/Components/Main/Automation/Categories.tsx
+++ dashboard/src/Components/Main/Automation/Categories.tsx
@@ -2,29 +2,20 @@ import { AddCircleOutline, DeleteOutline, Edit } from '@material-ui/icons';
 import React, { FC } from 'react';
 import { NavLink, useParams } from 'react-router-dom';
 
-const Categories: FC<any> = ({ category }) => {
-	let { categorylink }: { categorylink: string } = useParams();
-	categorylink = category;
-	console.log(categorylink);
+const Categories: FC<any> = ({ category, click, active }) => {
 	return (
 		<>
-			<NavLink
-				activeClassName="automation__category-list__item--active"
-				className="automation__category-list__item"
-				to={`automation/${categorylink.split(' ').join('-')}`}
-			>
-				<div className="automation__category-list__item__name">
-					{category}
-					<div className="automation__category-list__item__name__button-section">
-						<button className="automation__category-list__item__name__button-section__edit">
-							<Edit style={{ fontSize: 30 }} />
-						</button>
-						<button className="automation__category-list__item__name__button-section__delete">
-							<DeleteOutline style={{ fontSize: 30 }} />
-						</button>
-					</div>
+			<div className={`automation__category-list__item__name ${active}`} onClick={click}>
+				{category}
+				<div className="automation__category-list__item__name__button-section">
+					<button className="automation__category-list__item__name__button-section__edit">
+						<Edit style={{ fontSize: 30 }} />
+					</button>
+					<button className="automation__category-list__item__name__button-section__delete">
+						<DeleteOutline style={{ fontSize: 30 }} />
+					</button>
 				</div>
-			</NavLink>
+			</div>
 		</>
 	);
 };
--- dashboard/src/Pages/Automation/Automation.tsx
+++ dashboard/src/Pages/Automation/Automation.tsx
@@ -8,6 +8,7 @@ import { Route, Switch, useLocation, useParams } from 'react-router-dom';
 
 const Automation: FC = () => {
 	const [automations, setAutomations] = useState<IAutomation[] | null>(null);
+	const [showtopiclist, setShowtopiclist]: any | null = useState(null);
 
 	useEffect(() => {
 		if (!automations) {
@@ -30,6 +31,15 @@ const Automation: FC = () => {
 
 	console.log(faqcategorylist);
 
+	const handleCategoryClick = (category: string) => {
+		console.log('clicked ' + category);
+		const filteredcategorylist: any = automations?.filter((automation) => {
+			return automation.category.includes(category);
+		});
+		setShowtopiclist(filteredcategorylist);
+		console.log(showtopiclist);
+	};
+
 	return (
 		<div className="automation">
 			<section className="automation__category-list">
@@ -39,29 +49,37 @@ const Automation: FC = () => {
 				</h2>
 				{categorylist?.length &&
 					categorylist.map((category: string) => {
-						return <Categories key={category} category={category} />;
+						return (
+							<Categories
+								key={category}
+								category={category}
+								click={() => handleCategoryClick(category)}
+								active={
+									showtopiclist && showtopiclist[0]?.category[0] === category
+										? 'automation__category-list__item__name--active'
+										: ''
+								}
+							/>
+						);
 					})}
 			</section>
-			<div className="automation__topics-list">
-				<Switch>
-					<Route exact path="/automation">
-						<ul>
-							{faqcategorylist?.length &&
-								faqcategorylist.map((faq: IAutomation) => {
-									return <Topics key={faq.id} topic={faq.topic} />;
-								})}
-						</ul>
-					</Route>
 
-					<Route path="/automation/:categorylink">
-						<ul>
-							{automations?.length &&
-								automations.map((faq: IAutomation) => {
-									return <Topics key={faq.id} topic={faq.topic} />;
-								})}
-						</ul>
-					</Route>
-				</Switch>
+			<div className="automation__topics-list">
+				{!showtopiclist ? (
+					<ul>
+						{faqcategorylist?.length &&
+							faqcategorylist.map((faq: IAutomation) => {
+								return <Topics key={faq.id} topic={faq.topic} />;
+							})}
+					</ul>
+				) : (
+					<ul>
+						{showtopiclist?.length &&
+							showtopiclist.map((category: IAutomation) => {
+								return <Topics key={category.id} topic={category.topic} />;
+							})}
+					</ul>
+				)}
 			</div>
 		</div>
 	);
