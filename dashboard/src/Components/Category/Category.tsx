0032c56e0b5783d2f0421e63607572f9e3cc5252
Roman Tuomisto <romantuomisto@gmail.com>
2021-07-25 12:04:38 +0300

    creates Button component

--- dashboard/src/Components/Category/Category.tsx
+++ dashboard/src/Components/Category/Category.tsx
@@ -1,27 +1,19 @@
 import { DeleteOutline, Edit } from '@material-ui/icons';
-import axios from 'axios';
-import React, { FC, useEffect, useState } from 'react';
-import { IAutomation, ITopic } from '../../@types';
-import TransitionsModal from '../../Components/Shared/TransitionsModal';
+import React, { FC, useState } from 'react';
+import Modal from '../Modal/Modal';
 
 interface ICategoryProps {
 	category: string;
 	categoryClickHandler: (category: string) => void;
 	active: boolean;
-	deleteCategoryHandle: () => void;
+	deleteCategoryHandle: (category: string) => void;
 }
 
 const Category: FC<ICategoryProps> = ({ category, categoryClickHandler, active, deleteCategoryHandle }) => {
 	const [openModal, setOpenModal] = useState<boolean>(false);
-	const [categories, setCategories] = useState<string[] | null>(null);
-	const [topics, setTopics] = useState<ITopic[] | null>(null);
 
-	const handleOpenModal = () => {
-		setOpenModal(true);
-	};
-
-	const handleCloseModal = () => {
-		setOpenModal(false);
+	const handleModalState = () => {
+		setOpenModal(!openModal);
 	};
 
 	return (
@@ -34,19 +26,16 @@ const Category: FC<ICategoryProps> = ({ category, categoryClickHandler, active, 
 				<button className="automation__category-list__item__name__button-section__edit">
 					<Edit style={{ fontSize: 30 }} />
 				</button>
-				<button className="automation__category-list__item__name__button-section__delete" onClick={handleOpenModal}>
+				<button className="automation__category-list__item__name__button-section__delete" onClick={handleModalState}>
 					<DeleteOutline style={{ fontSize: 30 }} />
 				</button>
 			</div>
-			<TransitionsModal
-				heading={`Delete category: ${category}?`}
-				yes="Yes"
-				no="No"
+			<Modal
+				title={`Delete category: ${category}?`}
 				description=""
-				handleOpenModal={handleOpenModal}
-				handleCloseModal={handleCloseModal}
+				handleCloseModal={handleModalState}
 				open={openModal}
-				yesButtonHandle={() => deleteCategoryHandle()}
+				yesButtonHandle={() => deleteCategoryHandle(category)}
 			/>
 		</div>
 	);
---
+++ dashboard/src/Components/Modal/Modal.tsx
@@ -0,0 +1,47 @@
+import React, { FC } from 'react';
+import { Modal as MaterialModal } from '@material-ui/core';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import Button from '../Button/Button';
+
+interface IModalProps {
+	title: string;
+	description: string;
+	open: boolean;
+	handleCloseModal: () => void;
+	yesButtonHandle: () => void;
+}
+
+const Modal: FC<IModalProps> = ({ title, description, open, handleCloseModal, yesButtonHandle }) => {
+	return (
+		<MaterialModal
+			aria-labelledby="modal-title"
+			aria-describedby="modal-description"
+			className="modal"
+			open={open}
+			onClose={() => handleCloseModal()}
+			closeAfterTransition
+			BackdropComponent={Backdrop}
+			BackdropProps={{
+				timeout: 500,
+			}}
+		>
+			<Fade in={open}>
+				<div className="modal__paper">
+					<h2 className="modal__paper__title">{title}</h2>
+					<p className="modal__paper__description">{description}</p>
+					<div className="modal__paper__buttons">
+						<Button type="submit" handleClick={yesButtonHandle}>
+							Yes
+						</Button>
+						<Button type="button" handleClick={handleCloseModal}>
+							No
+						</Button>
+					</div>
+				</div>
+			</Fade>
+		</MaterialModal>
+	);
+};
+
+export default Modal;
