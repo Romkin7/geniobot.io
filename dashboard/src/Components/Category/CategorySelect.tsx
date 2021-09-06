155ca96448593f9a9ea9492c5ba44ef76a472e1d
Yuliia Matvi <jualiasha@gmail.com>
2021-09-06 21:04:16 +0300

    dropdown added as multiple select

--- dashboard/src/Components/Category/CategorySelect.tsx
+++ dashboard/src/Components/Category/CategorySelect.tsx
@@ -1,82 +1,52 @@
-import React, { FC } from 'react';
-import { FormControl, InputLabel, Select, Input, Chip, MenuItem } from '@material-ui/core';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-
-const useStyles = makeStyles((theme) => ({
-	paper: {
-		backgroundColor: 'rgba(32, 29, 63, 1)',
-		border: '1px solid #000',
-		boxShadow: theme.shadows[5],
-		padding: '5vw',
-		borderRadius: '20px',
-	},
-	formControl: {
-		margin: theme.spacing(1),
-		minWidth: '100%',
-		/* maxWidth: 300, */
-	},
-	chips: {
-		display: 'flex',
-		flexWrap: 'wrap',
-	},
-	chip: {
-		margin: 2,
-	},
-}));
-
-function getStyles(name: any, categoryName: any, theme: any) {
-	return {
-		fontWeight: categoryName.indexOf(name) === -1 ? theme.typography.fontWeightRegular : theme.typography.fontWeightMedium,
-	};
-}
+import React, { FC, FormEvent } from 'react';
+import { FormControl, InputLabel, Select } from '@material-ui/core';
+import Button from '../Button/Button';
 
 const CategorySelect: FC = () => {
-	const classes = useStyles();
 	const [categoryName, setcategoryName] = React.useState([]);
-	const theme = useTheme();
 
 	/*category names*/
 	const names = ['Internet', 'Telephone', 'Business', 'Private'];
-	const ITEM_HEIGHT = 48;
-	const ITEM_PADDING_TOP = 8;
-	const MenuProps = {
-		PaperProps: {
-			style: {
-				maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-				width: 250,
-			},
-		},
+
+	const handleChangeMultiple = (event: any) => {
+		const { options } = event.target;
+		const value: any = [];
+		for (let i = 0, l = options.length; i < l; i += 1) {
+			if (options[i].selected) {
+				value.push(options[i].value);
+			}
+		}
+		setcategoryName(value);
 	};
 
-	const handleChange = (event: any) => {
-		setcategoryName(event.target.value);
-	};
+	const popoverSubmit = (event: any) => {};
+
 	return (
-		<FormControl className={classes.formControl}>
-			<InputLabel id="demo-mutiple-chip-label"></InputLabel>
-			<Select
-				labelId="demo-mutiple-chip-label"
-				id="demo-mutiple-chip"
-				multiple
-				value={categoryName}
-				onChange={handleChange}
-				input={<Input id="select-multiple-chip" />}
-				renderValue={(selected: any) => (
-					<div className={classes.chips}>
-						{selected.map((value: any) => (
-							<Chip key={value} label={value} className={classes.chip} />
-						))}
-					</div>
-				)}
-				MenuProps={MenuProps}
-			>
-				{names.map((name) => (
-					<MenuItem key={name} value={name} style={getStyles(name, categoryName, theme)}>
-						{name}
-					</MenuItem>
-				))}
-			</Select>
-		</FormControl>
+		<div className="popover">
+			<FormControl className="popover__popover">
+				<InputLabel shrink htmlFor="select-multiple-native" className="popover__title">
+					Choose Categories
+				</InputLabel>
+				<Select
+					multiple
+					native
+					value={categoryName}
+					onChange={handleChangeMultiple}
+					inputProps={{
+						id: 'select-multiple-native',
+					}}
+				>
+					{names.map((name) => (
+						<option key={name} value={name} className="popover__category">
+							{name}
+						</option>
+					))}
+				</Select>
+			</FormControl>
+			<Button type="submit" variant="popover__button" handleClick={(event: FormEvent) => popoverSubmit(event)}>
+				Ok
+			</Button>
+		</div>
 	);
 };
 
--- dashboard/src/Components/Main/Intellect/QuestionBox.tsx
+++ dashboard/src/Components/Main/Intellect/QuestionBox.tsx
@@ -3,31 +3,42 @@ import { Edit, DeleteOutline } from '@material-ui/icons';
 import { AppContext } from '../../../store/appContext';
 import Button from '../../Button/Button';
 import CategorySelect from '../../Category/CategorySelect';
+import { makeStyles } from '@material-ui/core/styles';
+import Popover from '@material-ui/core/Popover';
+import Typography from '@material-ui/core/Typography';
 
 interface QuestioProps {
 	question: string;
 }
 
+const useStyles = makeStyles((theme) => ({
+	typography: {
+		padding: theme.spacing(2),
+	},
+}));
+
 const QuestionBox: FC<QuestioProps> = ({ question }) => {
 	const { setModalAction } = useContext(AppContext);
-	const { setPopoverAction, popover } = useContext(AppContext);
+
+	const classes = useStyles();
+	const [anchorEl, setAnchorEl] = React.useState(null);
+
+	const handleClick = (event: any) => {
+		setAnchorEl(event.currentTarget);
+	};
+
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+
+	const open = Boolean(anchorEl);
+	const id = open ? 'simple-popover' : undefined;
 
 	return (
 		<div className="intellect__question">
 			{question}
 			<div className="intellect__question__button-section">
-				<Button
-					type="button"
-					aria-describedby={Boolean(popover.anchorEl) ? 'simple-popover' : undefined}
-					variant="intellect__question__button-section__edit"
-					handleClick={(event: any) =>
-						setPopoverAction({
-							anchorEl: event.currentTarget,
-							content: <CategorySelect />,
-							id: 'simple-popover',
-						})
-					}
-				>
+				<Button type="button" aria-describedby={id} variant="intellect__question__button-section__edit" handleClick={handleClick}>
 					<Edit style={{ fontSize: 30 }} />
 				</Button>
 				<Button
@@ -45,6 +56,24 @@ const QuestionBox: FC<QuestioProps> = ({ question }) => {
 					<DeleteOutline style={{ fontSize: 30 }} />
 				</Button>
 			</div>
+			<Popover
+				id={id}
+				open={open}
+				anchorEl={anchorEl}
+				onClose={handleClose}
+				anchorOrigin={{
+					vertical: 'bottom',
+					horizontal: 'left',
+				}}
+				transformOrigin={{
+					vertical: 'top',
+					horizontal: 'right',
+				}}
+			>
+				<Typography className={classes.typography}>
+					<CategorySelect />
+				</Typography>
+			</Popover>
 		</div>
 	);
 };
